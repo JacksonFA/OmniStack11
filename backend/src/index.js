be8ac1eed55325a6af981673f21c83b71e7739ea
JacksonFA <jackson.f205@gmail.com>
2020-03-25 12:37:13 -0300

    Backend

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table(users).select('*').where()
+ */
+
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
